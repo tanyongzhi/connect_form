9fe23d2cf7069b34c21e58eb6f3d7cc6733dae95
Aspen Tng <aspentng@gmail.com>
2020-01-25 15:14:46 -0500

    basic buttons

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
 from constants import *
 import json
 
 app = Flask(__name__)
 
-@app.route("/") # we are using get method here
+
+@app.route("/")  # we are using get method here
 def index():
     return render_template("index.html")
 
-@app.route("/slacker") # we are using get method here
+@app.route("/slacker")  # we are using get method here
 def slacker_list():
     test = Slacker(4, "HL Lee", "6 to 9", (6, 9), (7, 10))
     test2 = Slacker(5, "HY Lee", "1 to 9", (1, 9), (6, 10))
@@ -16,7 +17,7 @@ def slacker_list():
     json_string = json.dumps([ob.__dict__ for ob in test_arr])
     print(json_string)
     return render_template("slacker_list.html", user=json_string)
-                    
+
+
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
-
