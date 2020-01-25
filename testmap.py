f45c5e1e57a275f098dd35695c9fd04ec4436fab
Tan Yong Zhi <yztan2@illinois.edu>
2020-01-25 18:08:24 -0500

    for gavin xoxo

---
+++ testmap.py
@@ -0,0 +1,10 @@
+import googlemaps
+
+gmaps_key = googlemaps.Client(key = "AIzaSyB0bPOg9jEX3s8c0QOcSDo3xN88L1HaKd8")
+
+geocode_result = gmaps_key.geocode("402 South Fifth Straeet, Champaign, IL")
+
+lat = geocode_result[0]["geometry"]["location"]["lat"]
+lng = geocode_result[0]["geometry"]["location"]["lng"]
+print("lat: " + str(lat))
+print("lng: " + str(lng))    
